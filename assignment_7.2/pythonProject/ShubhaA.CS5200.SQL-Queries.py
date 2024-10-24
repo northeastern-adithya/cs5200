44e6b30ad16f4d06b7484907b3a923fc7a3a9bf9
Adithya Aravind <adithya@Adithyas-MacBook-Pro.local>
2024-10-24 00:03:38 -0400

    feat: assignment 7.2

--- assignment_7.2/pythonProject/ShubhaA.CS5200.SQL-Queries.py
+++ assignment_7.2/pythonProject/ShubhaA.CS5200.SQL-Queries.py
@@ -1,5 +1,11 @@
 import sqlite3
 
+"""
+title: ASSIGNMENT 07.2: Query a Database with SQL in Python 
+author: Shubha, Adithya
+date: 2024-10-23
+
+"""
 
 
 def beautifyPrint(rs: list, columnNames: list):
@@ -10,9 +16,10 @@ def beautifyPrint(rs: list, columnNames: list):
         rs (list): The result set to be printed.
         columnNames (list): The list of column names for the result set.
     """
-    print(columnNames)
+    # referredFrom: https://stackoverflow.com/questions/12453580/how-to-concatenate-join-items-in-a-list-to-a-single-string
+    print("; ".join(map(str, columnNames)))
     for row in rs:
-        print(row)
+        print("; ".join(map(str, row)))
 
 
 def countriesWithMoreThanTenSuppliers(dbCon: sqlite3.Connection):
@@ -37,15 +44,27 @@ def countriesWithMoreThanTenSuppliers(dbCon: sqlite3.Connection):
             "Which countries have more than ten suppliers? List the country and the number of suppliers in each.")
         print("Number of countries with more than 10 suppliers: ", len(rs))
 
+        if (len(rs) > 10):
+            print("Countries with more than 10 suppliers")
+            columnNames = [description[0] for description in cursor.description]
+            beautifyPrint(rs, columnNames)
+            lineSeparator()
+            print("Fetching countries with their supplier count")
+        else:
+            print("No countries with more than 10 suppliers.Printing all countries with their supplier count")
+
         query = ('''SELECT S.Country, COUNT(S.SupplierID) AS TotalSuppliers
-                           FROM Suppliers S
-                           GROUP BY S.Country
-                           ORDER BY TotalSuppliers DESC''')
+                                       FROM Suppliers S
+                                       GROUP BY S.Country
+                                       ORDER BY TotalSuppliers DESC''')
         cursor = dbCon.cursor()
         cursor.execute(query)
         rs = cursor.fetchall()
         columnNames = [description[0] for description in cursor.description]
+
+        print("Countries with their supplier count")
         beautifyPrint(rs, columnNames)
+        cursor.close()
 
     except sqlite3.Error as exception:
         print("Error in fetching countries with more than 10 suppliers", exception)
@@ -63,7 +82,6 @@ def totalProductsSlodByEachSupplier(dbCon: sqlite3.Connection):
         query = ('''SELECT S.SupplierName, COUNT(DISTINCT P.ProductID) AS TotalProductsSold
                     FROM Suppliers S
                     JOIN Products P ON S.SupplierID = P.SupplierID
-                    JOIN OrderDetails OD ON P.ProductID = OD.ProductID
                     GROUP BY S.SupplierName
                     ORDER BY S.SupplierName''')
         cursor = dbCon.cursor()
@@ -73,6 +91,7 @@ def totalProductsSlodByEachSupplier(dbCon: sqlite3.Connection):
         rs = cursor.fetchall()
         columnNames = [description[0] for description in cursor.description]
         beautifyPrint(rs, columnNames)
+        cursor.close()
     except sqlite3.Error as exception:
         print("Error in fetching total products sold by each supplier", exception)
 
@@ -96,6 +115,7 @@ def fetchDetailsOfSuppliers(dbCon: sqlite3.Connection):
         rs = cursor.fetchall()
         columnNames = [description[0] for description in cursor.description]
         beautifyPrint(rs, columnNames)
+        cursor.close()
     except sqlite3.Error as exception:
         print("Error in fetching details of suppliers", exception)
 
@@ -117,6 +137,13 @@ def connectToDatabase(databaseName: str) -> sqlite3.Connection:
     return None
 
 
+def lineSeparator():
+    """
+    Prints a line separator.
+    """
+    print("________________________________________________________________________________________")
+
+
 def main():
     """
     Main function to execute the script. Connects to the database and fetches various details.
@@ -126,9 +153,14 @@ def main():
         return
     dbCon.execute("PRAGMA foreign_keys = ON")
     fetchDetailsOfSuppliers(dbCon)
+    lineSeparator()
     totalProductsSlodByEachSupplier(dbCon)
+    lineSeparator()
     countriesWithMoreThanTenSuppliers(dbCon)
+    lineSeparator()
+
     dbCon.close()
+
 
 # Code is referred from: http://artificium.us/lessons/07.python/l-7-300-primer-sqlite-py/l-7-300.html
 main()
